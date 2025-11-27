98e29b2c827930cdc288985e9ce84d79bcae14a4
Dennis Tereshkow <tereshkowdennis@gmail.com>
2025-11-27 21:46:18 +0300

    feat: add cli and main file

---
+++ csv_tool/main.py
@@ -0,0 +1,25 @@
+import sys
+
+from csv_tool.file_reader import read_csv
+from csv_tool.report_generator import generate_performance_report
+from csv_tool.validators import validate_files_exist
+from csv_tool.cli import parse_args
+
+
+def main():
+    try:
+        args = parse_args()
+        validate_files_exist(args.files)
+        data = read_csv(args.files)
+        if args.report == 'performance':
+            print(generate_performance_report(data))
+    except FileNotFoundError as e:
+        print(f"Ошибка: {e}")
+        sys.exit(1)
+    except Exception as e:
+        print(f"Неожиданная ошибка: {e}")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
